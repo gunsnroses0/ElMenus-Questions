7f6e9471a4d448c4c8226b8cf2edca82696ed615
ahmedash11 <ahmedashh11@gmail.com>
2019-04-26 22:07:24 +0200

    Update Question.java

--- src/Model/Question.java
+++ src/Model/Question.java
@@ -29,6 +29,7 @@ public class Question {
 
 	private static MongoCollection<Document> collection = null;
 	private static int DbPoolCount = 4;
+	static String host = System.getenv("MONGO_URI");
 	public static int getDbPoolCount() {
 		return DbPoolCount;
 	}
@@ -37,9 +38,10 @@ public class Question {
 	}
 	
 	public static HashMap<String, Object> create(HashMap<String, Object> atrributes) throws ParseException {
+		MongoClientOptions.Builder options = MongoClientOptions.builder()
+	            .connectionsPerHost(DbPoolCount);
 		MongoClientURI uri = new MongoClientURI(
-				"mongodb://localhost");
-
+				host,options);
 		MongoClient mongoClient = new MongoClient(uri);
 		MongoDatabase database = mongoClient.getDatabase("El-Menus");
 //    	Method method =   Class.forName("PlatesService").getMethod("getDB", null);
@@ -61,9 +63,10 @@ public class Question {
 	}
 	
 	public static HashMap<String, Object> update(String id, HashMap<String, Object> atrributes) {
+		MongoClientOptions.Builder options = MongoClientOptions.builder()
+	            .connectionsPerHost(DbPoolCount);
 		MongoClientURI uri = new MongoClientURI(
-				"mongodb://localhost");
-
+				host,options);
 		MongoClient mongoClient = new MongoClient(uri);
 		MongoDatabase database = mongoClient.getDatabase("El-Menus");
 //    	Method method =   Class.forName("PlatesService").getMethod("getDB", null);
@@ -85,8 +88,10 @@ public class Question {
 	}
 	
 	public static HashMap<String, Object> get(String messageId) {
+		MongoClientOptions.Builder options = MongoClientOptions.builder()
+	            .connectionsPerHost(DbPoolCount);
 		MongoClientURI uri = new MongoClientURI(
-				"mongodb://localhost");
+				host,options);
 		MongoClient mongoClient = new MongoClient(uri);
 		MongoDatabase database = mongoClient.getDatabase("El-Menus");
 //    	Method method =   Class.forName("PlatesService").getMethod("getDB", null);
